aca3bd9200d35d6319065c662a855d0399cdf16d
luqinthar <95601942+Keyz078@users.noreply.github.com>
2022-06-16 17:36:20 +0700

    Update change-kube-version-offline.sh

--- change-kube-version-offline.sh
+++ change-kube-version-offline.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ $1 == "" ]
+if [[ $1 == "" ]]
 then
     echo "Usage: ./change-kube-ver.sh <version>";
     exit
 fi
+source config.sh
 version=$1
-dir="roles/kubespray-defaults/defaults/main.yaml"
+dir="${KUBESPRAY_DIR}/roles/kubespray-defaults/defaults/main.yaml"
 sed -i "s/kube_version:.*/kube_version: ${version}/" "${dir}"
 echo version has changed to  $version
