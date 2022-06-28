18e206d4b87e708d6e9750e2f8f0103f14fe25e6
Keyz078 <luqinthar@gmail.com>
2022-06-29 02:12:52 +0700

    for updgrade cluster version

---
+++ upgrade-cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $1 == "" ] || [ $2 == "" ];
+then
+    echo "Usage: ./upgrade-c;uster.sh <inventory> <version>";
+    exit;
+fi
+inventory=$1
+version=$2
+echo "ansible-playbook cluster.yml -i ${inventory} -e kube_version=${version} -e upgrade_cluster_setup=true"
+
